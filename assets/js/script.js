c586496dfd1a61b6e628f5539e604ddf19a9288e
Echo <echo.dave@protonmail.com>
2019-10-23 22:41:56 -0400

    logic seems to be working aside from and odd math issue on the attach hp occasionally increasing

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,18 @@ $(document).ready(function () {
     let hero = false;
     let opponent = false;
 
+    let attackerIndex;
+    let deffenderIndex;
+
+    let bonusdmg = 0;
+
+
+    //hp for attacking
+    let attackHp
+    console.log(`hp ${attackHp}`)
+    let deffendHp
+    console.log(`defend ${deffendHp}`);
+
 
     //build character select
     function buildChars() {
@@ -28,7 +40,7 @@ $(document).ready(function () {
 
     buildChars();
 
-    $('#charBank div').on("click", function () {
+    $('#charBank>#character').on("click", function () {
         if (hero == false) {
             console.log("clicked" + $(this));
             //move to attacker box
@@ -36,24 +48,70 @@ $(document).ready(function () {
             $('#pick').remove();
             $('#wrap').prepend(`<h1 id="pick">Pick your opponent</h1>`);
             hero = true;
-       
+
         } else if (opponent == false) {
-           //move to defender box
+            //move to defender box
             $(this).appendTo("#defend");
             opponent = true;
-           
+
+//remove pick defender message
+$('#wrap > h1').remove();
+
             //make attack button
             $(`<div id="attack" class="btn">Attack!</div>`).insertBefore($('#attacker'));
-           
-            let attackHp = chars.health[parseInt($('#attacker #character').attr("value"))]
-            console.log(`hp ${attackHp}`)
-            let deffendHP = chars.health[parseInt($('#defend #character').attr("value"))]
-            console.log(`defend ${deffendHP}`);
+
+            // message to pick a new opponent for round 2 and 3
+            $('#defend + h1').remove();
+            //store hp values
+            attackHp = chars.health[parseInt($('#attacker #character').attr("value"))]
+            deffendHp = chars.health[parseInt($('#defend #character').attr("value"))]
+
+            attackerIndex = parseInt($('#attacker #character').attr("value"));
+            deffenderIndex = parseInt($('#defend #character').attr("value"));
+            console.log(`attack ${attackerIndex} deffend ${deffenderIndex}`);
+
+            //scope issue expected
+            /*            let attackHp = chars.health[parseInt($('#attacker #character').attr("value"))]
+                       console.log(`hp ${attackHp}`)
+                       let deffendHp = chars.health[parseInt($('#defend #character').attr("value"))]
+                       console.log(`defend ${deffendHp}`); */
         } else {
         }
-        $('#attack').on("click", function () {
 
+
+        $('#attack').on("click", function () {
+            console.log(`after attack hero ${hero}`)
             console.log(`$('#attacker #character').attr("value")`);
+
+            if (attackHp > 0 && deffendHp > 0) {
+
+                deffendHp = deffendHp - chars.Attack[attackerIndex] - bonusdmg;
+                bonusdmg = bonusdmg + chars.bonusAttack[attackerIndex];
+                console.log(bonusdmg + " bonus damage")
+                if (deffendHp <= 0) {
+                    deffendHp = 0;
+                    $(`#defend > #character`).remove();
+                    $('#attack').remove();
+
+                    if ($('#charBank > #character').length > 0) {
+                        $(`<h1> One down! Pick a new opponent!</h1>`).insertAfter($('#defend'))
+                        opponent = false;
+                        return // hero = true , opponent = true;
+                    } else {
+                        $('#wrap').prepend(`<div class="gameover">You Win</div>`);
+                    }
+                }
+
+                $('#defend #character li:nth-child(3)').html(deffendHp);
+                attackHp = attackHp - chars.Counter[deffenderIndex];
+                $('#attacker #character li:nth-child(3)').html(attackHp);
+                console.log(`def hp ${deffendHp}`);
+
+                if (attackHp <= 0) {
+                    attackHp = 0;
+                    $('#wrap').prepend(`<div class="gameover">better luck next time</div>`);
+                }
+            }
 
         })
 
